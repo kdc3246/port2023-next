5db518bfa46b432c77eeac370b06aaf02b149bea
kdc3246 <baba001109@naver.com>
2023-12-08 00:55:57 +0900

    Skill.jsx CHALLENGE 수정 중

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 import { skillText } from "@/constants";
 
@@ -14,7 +15,11 @@ const Skill = () => {
                         <div key={key}>
                             <span>{key+1}.</span>
                             <h3>{skill.title}</h3>
-                            <p>{skill.desc}</p>
+                            {/* <Link href={'https://wikidocs.net/5'} target="_blank">01. 파이썬이란?</Link><br />
+                            <Link href={'https://wikidocs.net/11'} target="_blank">02. 파이썬 프로그래밍의 기초, 자료형</Link><br />
+                            <Link href={'https://wikidocs.net/19'} target="_blank">03. 프로그램의 구조를 쌓는다! 제어문</Link><br />
+                            <Link href={'https://wikidocs.net/23'} target="_blank">04. 파이썬의 입출력</Link><br /> */}
+                            <p>{skill.desc}01. 파이썬이란?</p>
                         </div>
                     ))} 
                 </div>
